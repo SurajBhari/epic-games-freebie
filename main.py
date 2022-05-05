e602d90c3b7926580a58b66a53f3a73e7631cf6b
AG_1436 <surajbhari159@gmail.com>
2022-05-05 22:39:46 +0530

    Do not use dynamic XPATHS

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from selenium.common import exceptions
 from time import sleep
 
 file_name = "cookies.json"
-executable_path = r"/home/ag/cookies/geckodriver"
-driver = webdriver.Firefox(executable_path=executable_path)
+driver = webdriver.Firefox()
 
 with open(file_name, "r", encoding="UTF-8") as f:
 	cookie_dict = load(f)
@@ -28,35 +27,37 @@ driver.get_cookies()
 driver.get("https://www.epicgames.com/store/en-US/free-games")
 sleep(4)
 
-free_now =driver.find_element_by_xpath("//div[1]/div/div[4]/main/div[3]/div/div/div/div/div[2]/span/div/div/section/div/div[1]/div/div/a/div/div/div[1]/div[2]/span")
-if not free_now.text == "FREE NOW":
+try:
+
+	free_now = driver.find_elements_by_xpath("//span[text()='Free Now']")[0]
+except exceptions.NoSuchElementException:
 	quit("No free game available right now")
 
-free_now.click()
+driver.execute_script('arguments[0].click()', free_now)
 sleep(4)
 #18+ game check
 try:
-	continue_button = driver.find_element_by_xpath("//div[1]/div/div[4]/main/div[2]/div/div/div/div[1]/div/div/div/div[2]/div/button") 
+	continue_button = driver.find_element_by_xpath("//span[text()='Continue']")
 	if continue_button.text == "CONTINUE" : 
-		continue_button.click()
+		driver.execute_script('arguments[0].click()', continue_button)
 	else:
 		raise exceptions.NoSuchElementException
 except exceptions.NoSuchElementException:
 	pass
 
 sleep(4)
-add_to_cart = driver.find_element_by_xpath("//div[1]/div/div[4]/main/div[2]/div/div/div/div[2]/div[3]/div/aside/div/div/div[6]/div/button")
-add_to_cart.click()
+add_to_cart = driver.find_elements_by_xpath("//span[text()='Add To Cart']")[0] 
+driver.execute_script('arguments[0].click()', add_to_cart)
 sleep(4)
 driver.get("https://www.epicgames.com/store/en-US/cart")
 sleep(4)
-check_out = driver.find_element_by_xpath("//div[1]/div/div[4]/main/div[2]/div/div/div/div/section/div/div[3]/div[2]/div/div[6]/button")
-check_out.click()
+check_out = driver.find_elements_by_xpath("//span[text()='Check Out']")[0]
+driver.execute_script('arguments[0].click()', check_out)
 sleep(10) # Suppppper slow
 iframe = driver.find_element_by_xpath("//div[4]/iframe")
 driver.switch_to.frame(iframe)
-place_order = driver.find_element_by_xpath("//div[1]/div/div[4]/div/div/div/div[2]/div[2]/button/div/div/span")
-place_order.click()
+place_order = driver.find_elements_by_xpath("//span[text()='Place Order']")[0]
+driver.execute_script('arguments[0].click()', place_order)
 sleep(4)
 
 """
