fcc14221b4d0f8db68408d4094ae2bc998a45595
AG4lyf <surajbhari159@gmail.com>
2023-04-29 00:27:51 +0530

    black formatting

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ file_name = "cookies.json"
 driver = webdriver.Firefox()
 
 with open(file_name, "r", encoding="UTF-8") as f:
-	cookie_dict = load(f)
+    cookie_dict = load(f)
 
 
 driver.get(cookie_dict["url"])
 for cookie in cookie_dict["cookies"]:
-	if cookie["sameSite"] == "unspecified":
-		cookie["sameSite"] = "Lax"
-	elif cookie["sameSite"] == "no_restriction":
-		cookie["sameSite"] = "None"
-	else:
-		cookie["sameSite"] = "Strict"
-	
-	driver.add_cookie(cookie)
+    if cookie["sameSite"] == "unspecified":
+        cookie["sameSite"] = "Lax"
+    elif cookie["sameSite"] == "no_restriction":
+        cookie["sameSite"] = "None"
+    else:
+        cookie["sameSite"] = "Strict"
+
+    driver.add_cookie(cookie)
 
 driver.get_cookies()
 
@@ -29,35 +29,35 @@ sleep(4)
 
 try:
 
-	free_now = driver.find_elements_by_xpath("//span[text()='Free Now']")[0]
+    free_now = driver.find_elements_by_xpath("//span[text()='Free Now']")[0]
 except exceptions.NoSuchElementException:
-	quit("No free game available right now")
+    quit("No free game available right now")
 
-driver.execute_script('arguments[0].click()', free_now)
+driver.execute_script("arguments[0].click()", free_now)
 sleep(4)
-#18+ game check
+# 18+ game check
 try:
-	continue_button = driver.find_element_by_xpath("//span[text()='Continue']")
-	if continue_button.text == "CONTINUE" : 
-		driver.execute_script('arguments[0].click()', continue_button)
-	else:
-		raise exceptions.NoSuchElementException
+    continue_button = driver.find_element_by_xpath("//span[text()='Continue']")
+    if continue_button.text == "CONTINUE":
+        driver.execute_script("arguments[0].click()", continue_button)
+    else:
+        raise exceptions.NoSuchElementException
 except exceptions.NoSuchElementException:
-	pass
+    pass
 
 sleep(4)
-add_to_cart = driver.find_elements_by_xpath("//span[text()='Add To Cart']")[0] 
-driver.execute_script('arguments[0].click()', add_to_cart)
+add_to_cart = driver.find_elements_by_xpath("//span[text()='Add To Cart']")[0]
+driver.execute_script("arguments[0].click()", add_to_cart)
 sleep(4)
 driver.get("https://www.epicgames.com/store/en-US/cart")
 sleep(4)
 check_out = driver.find_elements_by_xpath("//span[text()='Check Out']")[0]
-driver.execute_script('arguments[0].click()', check_out)
-sleep(10) # Suppppper slow
+driver.execute_script("arguments[0].click()", check_out)
+sleep(10)  # Suppppper slow
 iframe = driver.find_element_by_xpath("//div[4]/iframe")
 driver.switch_to.frame(iframe)
 place_order = driver.find_elements_by_xpath("//span[text()='Place Order']")[0]
-driver.execute_script('arguments[0].click()', place_order)
+driver.execute_script("arguments[0].click()", place_order)
 sleep(4)
 
 """
@@ -69,6 +69,6 @@ Lets say it didn't. lets see what's next ?
 
 sleep(4)
 if "success" in driver.current_url:
-	print("VOILA")
+    print("VOILA")
 else:
-	print("Fucked")
+    print("Fucked")
